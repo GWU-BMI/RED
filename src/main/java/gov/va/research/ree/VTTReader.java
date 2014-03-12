8bab0d90664381cd53e8d687a847646afbfed8bd
Doug Redd <doug.redd@utah.edu>
2014-03-11 22:42:28 -0600

    Fixed early return without all triplets. Enlarged whitespace match to match up to 50 spaces.

--- src/main/java/gov/va/research/ree/VTTReader.java
+++ src/main/java/gov/va/research/ree/VTTReader.java
@@ -107,14 +107,17 @@ public class VTTReader {
 			replaceDigitsLS(ls3list);
 			Map<String,List<LSTriplet>> snippetGroups = groupSnippets(ls3list);
 			processSnippetGroups(snippetGroups);
+			List<LSTriplet> allTriplets = new ArrayList<>();
 			for(List<LSTriplet> tripletList : snippetGroups.values()){
 				replaceDigitsBLSALS(tripletList);
 				replaceWhiteSpaces(tripletList);
 				//replacePunctEnd(ls3list);
-				return tripletList;
+				//return tripletList;
+				allTriplets.addAll(tripletList);
 				/*for(LSTriplet triplet : tripletList)
 					regExpressions.add(triplet.toString());*/
 			}
+			return allTriplets;
 		}
 		return null;
 		/*for(String reEx: regExpressions){
@@ -328,13 +331,13 @@ public class VTTReader {
 		for(LSTriplet x : ls3list)
 		{
 			s=x.getBLS();
-			s=s.replaceAll("\\s+","\\\\s{1,10}");
+			s=s.replaceAll("\\s+","\\\\s{1,50}");
 			x.setBLS(s);
 			s=x.getLS();
-			s=s.replaceAll("\\s+","\\\\s{1,10}");
+			s=s.replaceAll("\\s+","\\\\s{1,50}");
 			x.setLS(s);
 			s=x.getALS();
-			s=s.replaceAll("\\s+","\\\\s{1,10}");
+			s=s.replaceAll("\\s+","\\\\s{1,50}");
 			x.setALS(s);
 		}
 		return ls3list;
